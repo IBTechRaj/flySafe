6500873e2eb8d6346e38d153bfe273e1297e582e
ibtechraj <raj_shk@rediffmail.com>
2020-03-11 00:44:37 +0530

    Fix linting issues

--- src/test/birdScream.test.js
+++ src/test/birdScream.test.js
@@ -1,8 +1,7 @@
-import { birdScream } from '../Scenes/GameScene';
+// import { birdScream } from '../Scenes/GameScene';
 
 test('checks if score is reduced by penalty points', () => {
   this.score = 10;
   this.penalty = 5;
   expect(this.netScore).toBe(10);
 });
-
